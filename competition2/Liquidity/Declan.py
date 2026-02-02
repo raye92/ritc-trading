54c11298ea911af845d8a2e8e7f6cc2daa9c209a
Declan Smith <write.declan.smith@gmail.com>
2026-02-02 00:29:06 -0500

    "initial work on Liquidy case. Figuring out API stuff for the tenders"

--- competition2/Liquidity/Declan.py
+++ competition2/Liquidity/Declan.py
@@ -17,16 +17,17 @@ s.headers.update({'X-API-key': APIKEY})
 #Sample setup
 MAX_EXPOSURE = 15000
 ORDER_LIMIT = 500
+BASEURL ='http://localhost:9999/v1/case'
 
 def get_tick():
-    resp = s.get('http://localhost:9999/v1/case')
+    resp = s.get(BASEURL)
     if resp.ok:
         case = resp.json()
         return case['tick'], case['status']
 
 def get_bid_ask(ticker):
     payload = {'ticker': ticker}
-    resp = s.get ('http://localhost:9999/v1/securities/book', params = payload)
+    resp = s.get (BASEURL + '/securities/book', params = payload)
     if resp.ok:
         book = resp.json()
         bid_side_book = book['bids']
@@ -47,7 +48,7 @@ def get_bid_ask(ticker):
 
 def get_time_sales(ticker):
     payload = {'ticker': ticker}
-    resp = s.get ('http://localhost:9999/v1/securities/tas', params = payload)
+    resp = s.get (BASEURL + '/securities/tas', params = payload)
     if resp.ok:
         book = resp.json()
         time_sales_book = [item["quantity"] for item in book]
@@ -55,7 +56,7 @@ def get_time_sales(ticker):
 
 def get_ind_position(ticker):
     payload = {'ticker': ticker}
-    resp = s.get('http://localhost:9999/v1/securities', params=payload)
+    resp = s.get(BASEURL + '/securities', params=payload)
     if resp.ok:
         securities = resp.json()
         for security in securities:
@@ -64,14 +65,14 @@ def get_ind_position(ticker):
     return 0  # Return 0 if ticker not found or request fails
 
 def get_position():
-    resp = s.get ('http://localhost:9999/v1/securities')
+    resp = s.get (BASEURL + '/securities')
     if resp.ok:
         book = resp.json()
-        return abs(book[0]['position']) + abs(book[1]['position']) + abs(book[2]['position']) + abs(book[3]['position'])
+        return abs(book[0]['position']) + abs(book[1]['position']) 
 
 def get_open_orders(ticker):
     payload = {'ticker': ticker}
-    resp = s.get ('http://localhost:9999/v1/orders', params = payload)
+    resp = s.get (BASEURL + '/orders', params = payload)
     if resp.ok:
         orders = resp.json()
         buy_orders = [item for item in orders if item["action"] == "BUY"]
@@ -79,44 +80,29 @@ def get_open_orders(ticker):
         return buy_orders, sell_orders
 
 def get_order_status(order_id):
-    resp = s.get ('http://localhost:9999/v1/orders' + '/' + str(order_id))
+    resp = s.get (BASEURL + '/orders' + '/' + str(order_id))
     if resp.ok:
         order = resp.json()
         return order['status']
+    
+def get_tenders():
+    resp = s.get(BASEURL+ '/tenders')
+    if resp.ok:
+        return resp.json()
+    
+def flattenPosiitons():
+     resp = requests.post(BASEURL + '/tenders', params={"all": 1})
+
+
+    
+    
 
 def main():
     tick, status = get_tick()
-    ticker_list = [i['ticker'] for i in s.get('http://localhost:9999/v1/securities').json()]
+    ticker_list = [i['ticker'] for i in s.get(BASEURL+ '/securities').json()]
 
     while status == 'ACTIVE':     
         
-        """closes all the positions at every 55 tick (in a minute) to avoid the position penalty"""   
-        if tick % 60 >= 55:
-            for ticker_symbol in ticker_list:
-                position = get_ind_position(ticker_symbol) 
-
-                if position > 0:
-                    s.post('http://localhost:9999/v1/orders', params={'ticker': ticker_symbol, 'type': 'MARKET', 'quantity': position, 'action': 'SELL'})
-                elif position < 0:
-                    s.post('http://localhost:9999/v1/orders', params={'ticker': ticker_symbol, 'type': 'MARKET', 'quantity': abs(position), 'action': 'BUY'})
-
-        for i in range(len(ticker_list)):
-            
-            ticker_symbol = ticker_list[i]
-            position = get_position()
-            best_bid_price, best_ask_price = get_bid_ask(ticker_symbol)
-
-            # Skip if book is empty
-            if best_bid_price is None or best_ask_price is None:
-                continue
-
-            if position < MAX_EXPOSURE:
-                resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT, 'price': best_bid_price, 'action': 'BUY'})
-                resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': ticker_symbol, 'type': 'LIMIT', 'quantity': ORDER_LIMIT, 'price': best_ask_price, 'action': 'SELL'})
-
-            sleep(0.5) 
-
-            s.post('http://localhost:9999/v1/commands/cancel', params = {'ticker': ticker_symbol})
 
         tick, status = get_tick()
 
